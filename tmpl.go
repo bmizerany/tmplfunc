353cf6c58fafab001c4438b705bd51474dd2c1da
Blake Mizerany <blake.mizerany@gmail.com>
2024-12-13 15:32:44 -0800

    tmplfunc: updated copy of website/tmplfunc

--- tmpl.go
+++ tmpl.go
@@ -18,7 +18,7 @@
 //
 //	{{template "link" (dict "url" "https://golang.org" "text" "the Go language")}}
 //
-// Function Definitions
+// # Function Definitions
 //
 // The function installed for a given template depends on the name of the
 // defined template, which can include not just a function name but also
@@ -52,9 +52,9 @@
 //
 //	{{define "link url text?"}}<a href="{{.url}}">{{or .text .url}}</a>{{end}}
 //
-// 	The Go home page is {{link "https://golang.org"}}.
+//	The Go home page is {{link "https://golang.org"}}.
 //
-// Usage
+// # Usage
 //
 // This package is meant to be used with templates from either the
 // text/template or html/template packages. Given a *template.Template
@@ -81,7 +81,9 @@ package tmplfunc
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io/fs"
+	"os"
+	"path"
 	"path/filepath"
 
 	htmltemplate "html/template"
@@ -116,6 +118,30 @@ func Parse(t Template, text string) error {
 // ParseFiles is like t.ParseFiles(filenames...), adding functions for the parsed templates.
 func ParseFiles(t Template, filenames ...string) error {
 	return parseFiles(t, readFileOS, filenames...)
+}
+
+// ParseFS is like ParseFiles or ParseGlob but reads from the file system fs
+// instead of the host operating system's file system.
+// It accepts a list of glob patterns.
+// (Note that most file names serve as glob patterns matching only themselves.)
+// It of course adds functions for the parsed templates.
+func ParseFS(t Template, fs fs.FS, patterns ...string) error {
+	return parseFS(t, fs, patterns)
+}
+
+func parseFS(t Template, fsys fs.FS, patterns []string) error {
+	var filenames []string
+	for _, pattern := range patterns {
+		list, err := fs.Glob(fsys, pattern)
+		if err != nil {
+			return err
+		}
+		if len(list) == 0 {
+			return fmt.Errorf("template: pattern matches no files: %#q", pattern)
+		}
+		filenames = append(filenames, list...)
+	}
+	return parseFiles(t, readFileFS(fsys), filenames...)
 }
 
 // parseFiles is the helper for the method and function. If the argument
@@ -208,6 +234,14 @@ func MustParseGlob(t Template, pattern string) {
 
 func readFileOS(file string) (name string, b []byte, err error) {
 	name = filepath.Base(file)
-	b, err = ioutil.ReadFile(file)
+	b, err = os.ReadFile(file)
 	return
+}
+
+func readFileFS(fsys fs.FS) func(string) (string, []byte, error) {
+	return func(file string) (name string, b []byte, err error) {
+		name = path.Base(file)
+		b, err = fs.ReadFile(fsys, file)
+		return
+	}
 }
